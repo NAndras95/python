ecb0d12555b46f4467cebd9cb3b96d65ce25c6db
Andras Nagy <andras.nagy@shiwaforce.com>
2021-01-28 11:15:00 +0100

    do_something update

--- log/log.py
+++ log/log.py
@@ -2,10 +2,13 @@
 
 import logging
 
+def do_something():
+    logging.info('Doing something')
+
 def main():
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
     logging.info('Started')
-#   do_something()
+    do_something()
     logging.info('Finished')
 
 if __name__ == '__main__':
